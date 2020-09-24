7b5dff3898cb717528a78395d87a29246b80a2cb
JCZou <jiachi.zou@gmail.com>
2020-09-24 21:41:12 +0200

    modify blog parse script

--- utils/log_parser/parse_blog.m
+++ utils/log_parser/parse_blog.m
@@ -1,6 +1,12 @@
 
 %% open log file
 [file, folder] = uigetfile({'*.bin'; '*.log'});
+
+if isequal(file,0)
+    % click cancel
+    return;    
+end
+
 logfile = strcat(folder, file);
 
 [header, log_msg] = blog_parser(logfile);
